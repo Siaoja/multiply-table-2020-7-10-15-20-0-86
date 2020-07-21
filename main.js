9c4d685e615b1888c80cb8cfc76c5823306a970b
SPIKE LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUSP@OOCL.COM>
2020-07-21 16:22:29 +0800

    [multipleTable][Spike Lu][feature]

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 function createMultiplyTable(start, end) {
+  if(isNumberValid(start,end)){
+      return calculateMultiplyTable(start,end);
+  }
   return null;
 }
+function isNumberValid(start, end){
+  if(start <= end && start >=1 && start <= 1000 && end >=1 && end <= 1000 ){
+    return true;
+  }
+  return false;
+}
 
+function calculateMultiplyTable(start, end){
+  let multiplyTable = "";
+  for(let i = start; i <= end; i++){//行数
+    multiplyTable += calculateMultiplyRow(start,i);
+    if(i != end){
+      multiplyTable += "\n";
+    }
+  }
+  console.log(multiplyTable);
+  return multiplyTable;
+}
+function calculateMultiplyRow(rowStart, rowEnd){
+  let multiplyRow = "";
+  for(let i = rowStart; i <= rowEnd; i++){//每一行的列数
+    let multiplyExpression = "" + i + "*" + rowEnd + "=" + (i*rowEnd);
+    if(i != rowEnd){
+      multiplyExpression += "\t";
+    }
+    multiplyRow += multiplyExpression ;
+  }
+  return multiplyRow;
+}
 module.exports = {
   createMultiplyTable,
 };
